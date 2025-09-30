62e8d9fd27f93c5fecfd50cbd8aba7dcf91cfea5
Deimian Vásquez <deimianvasquez@gmail.com>
2025-09-30 00:35:32 +0000

    Welcome python

---
+++ app.py.py
@@ -0,0 +1,84 @@
+# name = "Deimian"
+# last_name = "Vásquez"
+# age = 22
+# heigth = 99.00
+
+
+
+# print(name.upper(), last_name.lower())
+# print(type(heigth + age))
+# print(type(heigth))
+
+
+
+
+# String --> Esto es un comentario
+
+# saludo = "Hola externocleidomastoide"
+# despedida = "Chao"
+
+# despedida_replace = despedida.replace("a", "x")
+# print(saludo +" "+ despedida_replace)
+# print(saludo.count("e"))
+
+# doc_sctring = """ 
+#     Aquí podemos escribir documentación
+# """
+
+# print(f"Este texto es un saludo {saludo} {despedida}")
+
+# None --> Es pra indicar que algo no existe
+
+# Lista en python es un Array en javascript
+
+# names = ["Deimian", 52, True, {"age":10}]
+
+# print(type(names))
+# print(len(names))
+
+
+# Object => class
+# class Car():
+#     def __init__(self, name):
+#         self.name = name
+
+
+# car_one = Car("Chevrolet")
+# print(car_one.name)
+
+
+# # Esto es un diccionario
+# obj = {
+#     "name": "deimian",
+#     "lastname": "Vásquez"
+# }
+# print(obj["name"])
+
+
+# tuples --> son inmutables
+# names_tuples = ("Deimian", "María")
+# names_list = ["Deimian", "María"]
+
+# names_list[0] = "Juanita"
+# # names_tuples[0] = "Juanita Tuples"
+
+# name = "deimian"
+
+# print(names_tuples)
+# print(names_list)
+# print(name)
+
+
+
+# sets --> Teoria de conjunto
+
+names = {"Juan", "Pedro", "María"}
+names_two = {"juan", "Deimian","Daniel"}
+
+print(names)
+
+
+# import React from "react"
+
+
+
